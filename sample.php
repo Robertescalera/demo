841f2028d9d3d3a34e7e847fcc78e1521d3a2f91
Robertescalera <130377163+Robertescalera@users.noreply.github.com>
2024-11-27 11:00:26 +0800

    Create sample.php

---
+++ sample.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sample</title>
+</head>
+<body>
+    <h1>This is a sample</h1>
+
+    <div>
+        <h3>Type</h3>
+        <input type="text">
+    </div>
+</body>
+</html>
